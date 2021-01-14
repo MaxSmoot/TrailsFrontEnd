12df912ad3c52b606f9f782e43ca2b3c349225d3
Max Smoot <smootmw@appstate.edu>
2021-01-13 22:24:40 -0500

    fixed env variable naming

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from './store';
-console.log(process.env.VUE_APP_PROD_API_URL)
+
 
 createApp(App).use(store)
   .use(router)
